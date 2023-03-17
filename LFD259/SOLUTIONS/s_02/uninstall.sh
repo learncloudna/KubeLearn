23c4eea1b416daba86ad830651e4c673745749b5
Ubuntu <ubuntu@ip-10-0-0-183.us-west-2.compute.internal>
2023-03-17 00:59:50 +0000

    before 2023-02-28

--- LFD259/SOLUTIONS/s_02/uninstall.sh
+++ LFD259/SOLUTIONS/s_02/uninstall.sh
@@ -1,4 +1,9 @@
 sudo apt remove containerd.io kubelet kubeadm kubectl kubernetes-cni
 sudo rm -rf /var/lib/etcd 
 sudo rm -rf /etc/kubernetes/
+sudo rm -rf /opt/cni/bin
 sudo rm -rf /k8scp_run
+sudo rm -rf helm-v3.9.0-linux-amd64.tar.gz*
+sudo rm -rf crictl-v1.26.0-linux-amd64.tar.gz*
+sudo rm -rf linux-amd64/
+sudo apt autoremove
